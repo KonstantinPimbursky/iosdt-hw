0b417d287dc54376a81d94eef23cad67ced4ce98
Konstantin Pimbursky <konstantin.pimbursky@gmail.com>
2021-05-22 14:01:51 +0500

    Initial commit

---
+++ DataStorage/DataStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataStorage
+//
+//  Created by Konstantin Pimbursky on 22.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
